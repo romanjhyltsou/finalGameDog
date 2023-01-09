7ba4e38add147b91ea0212afabba9cc0e1376fff
romanjhyltsou <romanjhyltsou@gmail.com>
2023-01-09 22:55:38 +0300

    feat: Sitting, Running, Jumping, Falling

--- js/input.js
+++ js/input.js
@@ -1,38 +1,26 @@
-/* export default class InputHandler {
-    constructor() {
-      this.lastKey = '';
-      window.addEventListener('keydown', (e) =>{
-        console.log(e.key);
-        switch(e.key){
-            case "ArrowLeft":
-                this.lastKey = "PRESS left";
-                break;
-            case "ArrowRight":
-                this.lastKey = "PRESS right";
-                break;
-            case "ArrowDown":
-                this.lastKey = "PRESS down";
-                break;
-            case "ArrowUp":
-                this.lastKey = "PRESS up";
-                break;
-        }
-      });
-      window.addEventListener('keyup', (e) =>{
-        switch(e.key){
-            case "ArrowLeft":
-                this.lastKey = "RELEASE left";
-                break;
-            case "ArrowRight":
-                this.lastKey = "RELEASE right";
-                break;
-            case "ArrowDown":
-                this.lastKey = "RELEASE down";
-                break;
-            case "ArrowUp":
-                this.lastKey = "RELEASE up";
-                break;
-        }
-      });
+export class InputHandler {
+    constructor () {
+        this.keys = [];
+        window.addEventListener('keydown', e => {
+            if((e.key === 'ArrowDown' || 
+                e.key === 'ArrowUp' || 
+                e.key === 'ArrowLeft' || 
+                e.key === 'ArrowRight' || 
+                e.key === 'Enter'
+                ) && this.keys.indexOf(e.key) === -1) {
+                this.keys.push(e.key);
+            }
+            console.log(e.key, this.keys);
+        });
+        window.addEventListener('keyup', e => {
+            if(      e.key === 'ArrowDown' || 
+                     e.key === 'ArrowUp' || 
+                     e.key === 'ArrowLeft' || 
+                     e.key === 'ArrowRight' || 
+                     e.key === 'Enter'){
+                this.keys.splice(this.keys.indexOf(e.key), 1);
+            }
+            console.log(e.key, this.keys);
+        });
     }
-} */
+}
