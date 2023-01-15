527d4f4443da8f4029be048b66f7c932153b648a
romanjhyltsou <romanjhyltsou@gmail.com>
2023-01-15 18:10:02 +0300

    feat: made Particle and Dust

--- js/player.js
+++ js/player.js
@@ -1,4 +1,4 @@
-import { Sitting, Running, Jumping, Falling } from "./playerStates.js";
+import { Sitting, Running, Jumping, Falling, Rolling } from "./playerStates.js";
 export class Player {
     constructor(game){
         this.game = game;
@@ -17,9 +17,9 @@ export class Player {
         this.frameTimer = 0;
         this.speed = 0;
         this.maxSpeed = 10;
-        this.states = [new Sitting(this), new Running(this), new Jumping(this), new Falling(this)];
-        this.currentState = this.states[0];
-        this.currentState.enter();
+        this.states = [new Sitting(this.game), new Running(this.game),
+        new Jumping(this.game), new Falling(this.game), new Rolling(this.game)];
+
     }
     update(input, deltaTime) {
         this.checkCollision();
