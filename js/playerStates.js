7be9b1954b5b4f2a3a9ed62b373d622e988356bc
romanjhyltsou <romanjhyltsou@gmail.com>
2023-01-15 19:07:19 +0300

    feat: Diving State

--- js/playerStates.js
+++ js/playerStates.js
@@ -78,6 +78,8 @@ export class Jumping extends State {
             this.game.player.setState(states.FALLING, 1);
         }else if (input.includes('Enter')){
             this.game.player.setState(states.ROLLING, 2);
+        } else if (input.includes('ArrowDown')){
+            this.game.player.setState(states.DIVING, 0);
         }
     }
 }
@@ -96,6 +98,8 @@ export class Falling extends State {
     handleInput(input){
         if (this.game.player.onGround()){
             this.game.player.setState(states.RUNNING, 1);
+        } else if (input.includes('ArrowDown')){
+            this.game.player.setState(states.DIVING, 0);
         }
     }
 }
@@ -121,6 +125,29 @@ export class Rolling extends State {
             this.game.player.setState(states.FALLING, 1);
         }else if (input.includes('Enter') && input.includes('ArrowUp') && this.game.player.onGround()){
             this.game.player.vy -= 27;
+        }
+    }
+}
+
+export class Diving extends State {
+    constructor(game) {
+        super('DIVING' , game);
+    }
+
+    enter() {
+        this.game.player.frameX = 0;
+        this.game.player.maxFrame = 6;
+        this.game.player.frameY = 6;
+        this.game.player.vy = 15;
+    }
+
+    handleInput(input){
+        this.game.particles.unshift(new Fire(this.game, this.game.player.x + 
+            this.game.player.width * 0.5, this.game.player.y + this.game.player.height * 0.5));
+        if (this.game.player.onGround()){
+            this.game.player.setState(states.RUNNING, 1);
+        }else if (input.includes('Enter') && this.game.player.onGround()){
+            this.game.player.setState(states.ROLLING, 2);
         }
     }
 }
